1c7d9cce50fe65d6587cf6fecdc7d9386accfe62
Kiet <082.hoangtuankiet@gmail.com>
2024-07-23 14:32:35 +0700

    enabling CORS for front-end

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,15 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Enabling CORS
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor(new Reflector()));
   app.use(cookieParser());
